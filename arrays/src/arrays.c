c423d3d266dd0a14a65a0269e2e5d00533079804
Kean <kjmb84@mail.missouri.edu>
2016-01-28 22:53:28 -0600

    arrays.c completed save for array_locate. Trying to figure out the best approach for that one. Just started failing into the bitmaps, so I figured it'd be a good time to commit before I got too frustrated to figure out how.

--- arrays/src/arrays.c
+++ arrays/src/arrays.c
@@ -1,32 +1,93 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #include "../include/arrays.h"
 
 // LOOK INTO MEMCPY, MEMCMP, FREAD, and FWRITE
 
 bool array_copy(const void *src, void *dst, const size_t elem_size, const size_t elem_count) {
-
-	return false;
+	if (elem_size == 0 || elem_count == 0 || !src || !dst) //check to make sure input parameters are valid
+		return false; //if fails, return false
+	else {
+        //FILE * src_fp;
+        //FILE * dst_fp;
+        //src_fp = fopen(*src, "r");
+        //dst_fp = fopen(dst, "w");
+        /*if (elem_count == fread(dst, elem_size, elem_count, src)) //reads from src array to dst array
+            return true; //function success*/
+        if (memcpy(dst, src, elem_count*elem_size))
+            return true;
+    }
+    return false; //function failure
 }
 
 bool array_is_equal(const void *data_one, void *data_two, const size_t elem_size, const size_t elem_count) {
-
+    if (!data_one || !data_two || elem_count == 0 || elem_size == 0)
+        return false;
+    else {
+        if (memcmp(data_one, data_two, elem_count*elem_size) == 0)
+            return true;
+    }
 	return false;
 }
 
-ssize_t array_locate(const void *data, const void *target, const size_t elem_size, const size_t elem_count) { 
+ssize_t array_locate(const void *data, const void *target, const size_t elem_size, const size_t elem_count) {
+    if (!data || !target || elem_count == 0 || elem_size == 0)
+        return -1;
+    else {
+        for (size_t i = 0;i < elem_count;i++) {
+            //if (memcmp(data[elem_size*i], target, elem_size) == 0)
+            if (memcmp(data, target, elem_size) == 0)
+                return elem_size*i;
+        }
 
-	return 0;
+        //ssize_t point = 0;
+        /*for (int i = 0;i < elem_count;i++) {
+            if (memcmp(data[(ssize_t)i*elem_size], target, elem_size) == 0)
+                return i*elem_size;
+            //point += (ssize_t)elem_size;
+        }*/
+    }
+    return -1;
 }
 
 bool array_serialize(const void *src_data, const char *dst_file, const size_t elem_size, const size_t elem_count) {
-
-	return false;
+    if (!src_data || !dst_file || isspace(*dst_file) != 0 || elem_count == 0 || elem_size == 0)
+        return false;
+    /*else {
+        for (int i = 0;i < elem_count;i++) {
+            (memcpy(dst_file[i], src_data[i],elem_size)
+        }
+        return true;*/
+    else {
+        //FILE * src_fp;
+        FILE * dst_fp;
+        //src_fp = fopen(src_data, "r");
+        dst_fp = fopen(dst_file, "w");
+        if (dst_fp == NULL)
+            return false;
+        if (elem_count == fwrite(src_data, elem_size, elem_count, dst_fp))
+            fclose(dst_fp);
+            //fclose(src_fp);
+            return true;
+    }
+    return false;
 }
 
 bool array_deserialize(const char *src_file, void *dst_data, const size_t elem_size, const size_t elem_count) {
-	
+    if (!src_file || !dst_data || elem_count == 0 || elem_size == 0)
+        return false;
+    else {
+        FILE * src_fp;
+        //FILE * dst_fp;
+        src_fp = fopen(src_file, "r");
+        if (src_fp == NULL)
+            return false;
+        //dst_fp = fopen(dst_data, "w");
+        if (elem_count == fread(dst_data, elem_size, elem_count, src_fp))
+            return true;
+    }
 	return false;
 }
--- bits/src/bitmap.c
+++ bits/src/bitmap.c
@@ -1,17 +1,20 @@
 #include "../include/bitmap.h"
 
 // data is an array of uint8_t and needs to be allocated in bitmap_create
-//      and used in the remaining bitmap functions. You will use data for any bit operations and bit logic   
+//      and used in the remaining bitmap functions. You will use data for any bit operations and bit logic
 // bit_count the number of requested bits, set in bitmap_create from n_bits
-// byte_count the total number of bytes the data contains, set in bitmap_create 
-struct bitmap {
+// byte_count the total number of bytes the data contains, set in bitmap_create
+typedef struct bitmap {
 	uint8_t *data;
 	size_t bit_count, byte_count;
 };
 
 bitmap_t *bitmap_create(size_t n_bits) {
-
-	return NULL;
+    if (n_bits == 0)
+        return NULL;
+    bitmap new_bitmap = new bitmap;
+    new_bitmap = malloc (sizeof(bitmap));
+    new_bitmap.bit_count = n_bits;
 }
 
 bool bitmap_set(bitmap_t *const bitmap, const size_t bit) {

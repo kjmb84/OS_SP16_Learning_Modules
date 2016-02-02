776c84558cf4741bfe095a77b208cdadbdf3f841
Kean <kjmb84@mail.missouri.edu>
2016-02-01 19:31:10 -0600

    Worked on strings a bit more. string_length, string_equal now finished. I was unable to get string_duplicate correctly on either the first or second try, so onto the backburner it goes.

--- strings/src/sstring.c
+++ strings/src/sstring.c
@@ -16,19 +16,49 @@ bool string_valid(const char *str, const size_t length) {
 }
 
 char *string_duplicate(const char *str, const size_t length) {
+    //char *dst_str[length];
+    if (!str || length < 1)
+        return NULL;
+    //char *dst_str[length];
+    else
+        return NULL;
+        /*char dst_str[length];
+        strncpy(dst_str, str, length);
+        dst_str[length+1] = '\0';
+        return dst_str;
+
+        memcpy(dst_str, str, sizeof(char) * length);
+        dst_str += '\0';*/
+        /*for (size_t i=0;i < length;i++) {
+            //dst_str[i] = str[i];
+
+        }*/
+
 
 	return NULL;
 
 }
 
 bool string_equal(const char *str_a, const char *str_b, const size_t length) {
-
-	return false;
+    if (!str_a || !str_b || length < 1)
+        return false;
+    else {
+        if (strcmp(str_a, str_b) != 0)
+            return false;
+        return true;
+    }
 }
 
 int string_length(const char *str, const size_t length) {
-
-	return 0;
+    if (!str || length < 1)
+        return -1;
+    else {
+        for (int i = 0;i < length;i++) {
+            if (str[i] == '\0')
+                return i;
+        }
+    }
+    return -1;
 }
 
 int string_tokenize(const char *str, const char *delims, const size_t str_length,char **tokens, const size_t max_token_length, const size_t requested_tokens) {

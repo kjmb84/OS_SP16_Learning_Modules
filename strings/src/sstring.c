f1fd84aa0f88b58909975b00f11207b421183926
Kean <kjmb84@mail.missouri.edu>
2016-02-02 22:22:07 -0600

    Mostly finished sstring.c, sting_tokenizer is coming along but still returns garbage values for half of the tokens. string_to_int to be worked on next

--- strings/src/sstring.c
+++ strings/src/sstring.c
@@ -64,10 +64,30 @@ int string_length(const char *str, const size_t length) {
 int string_tokenize(const char *str, const char *delims, const size_t str_length,char **tokens, const size_t max_token_length, const size_t requested_tokens) {
     if (!str || !delims || str_length < 1 || !tokens || max_token_length < 1 || requested_tokens < 1)
         return 0;
-    else
-        for (size_t i = 0;i < str_length;i++) {
+    else {
 
+        for (int i = 0;i < requested_tokens;i++)
+            if (!tokens[i])
+                return -1;
+        /*
+        int i=0;
+        tokens[i++] = strtok(str, delims);
+        while (!tokens)
+            tokens[i++] = strtok(NULL, delims);
+        return i;
+    */
+
+        size_t toks = 0;
+        size_t since_tok = 0;
+        for (size_t i = 0;i < str_length;i++, ++since_tok) {
+            if (str[i] == *delims) {
+                strncpy(tokens[toks++], &str[i-since_tok], since_tok);
+                since_tok = 0;
+                printf("\n%s\n",tokens[toks-1]);
+            }
         }
+        return toks;
+    }
 }
 
 bool string_to_int(const char *str, int *converted_value) {
